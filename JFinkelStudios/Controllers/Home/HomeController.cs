15792b1bd82642280a394ee22c518b8080648980
Jacob Finkel <46610866+JFinkel7@users.noreply.github.com>
2020-02-07 15:02:12 -0600

    Add project files.

---
+++ JFinkelStudios/Controllers/Home/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace JFinkelStudios.Controllers.Home {
+    public class HomeController : Controller {
+
+
+        [HttpGet]
+        public IActionResult Index() {
+            return View();
+
+        }
+        // ADDED 
+        [HttpGet]
+        public IActionResult About() {
+            return View();
+
+        }
+
+        // ADDED 
+        [HttpGet]
+        public IActionResult HowIStarted() {
+            return View();
+
+        }
+
+        // ADDED 
+        [HttpGet]
+        public IActionResult Resume() {
+            return View();
+
+        }
+
+    }
+}
---
+++ JFinkelStudios/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace JFinkelStudios {
+    public class Startup {
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllersWithViews();
+        }
+
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{Controller=Home}/{Action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
